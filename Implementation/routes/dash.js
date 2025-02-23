5e2863eb32635a1ae4cd796b3235a718edb9f2de
Nidhi Nishad <nidhinishad21@gmail.com>
2025-02-23 01:46:05 +0000

    Adding add expense button

--- Implementation/routes/dash.js
+++ Implementation/routes/dash.js
@@ -5,8 +5,27 @@
 
 router.get('/', verifyToken, async(req, res) => {
     let firstname = req.firstname;
-    res.render("dashboard/home.ejs" , { firstname: firstname});
+    res.render("dashboard/home.ejs" , { path: '/dashboard', firstname: firstname});
  });
+
+ router.get('/insights', verifyToken, (req, res) => {
+    res.render('dashboard/home.ejs', {
+        path: '/dashboard/insights',
+        firstname: req.firstname
+    });
+});
+router.get('/transactions', verifyToken, (req, res) => {
+    res.render('dashboard/home.ejs', {
+        path: '/dashboard/transactions',
+        firstname: req.firstname
+    });
+});
+router.get('/categories', verifyToken, (req, res) => {
+    res.render('dashboard/home.ejs', {
+        path: '/dashboard/categories',
+        firstname: req.firstname
+    });
+});
 
 
  module.exports = router;
